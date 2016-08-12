3ebd2a69bb0457d461fb34fc7900b913ad3866a7
Bojie Jiang <jiangbojie@gmail.com>
2016-08-12 13:27:46 -0700

    setup the project

---
+++ app/src/main/java/us/bojie/testing_android/OtherActivity.java
@@ -0,0 +1,12 @@
+package us.bojie.testing_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
